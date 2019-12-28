189ae8fc3f50f1018ba756d34ed7366315f88f20
neomaster <x-cyber@windowslive.com>
2019-12-29 00:11:03 +0700

    update hadoop upload process at client side

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
@@ -18,6 +19,17 @@ namespace aspcorehadoopupload.Controllers
         readonly string m_HadoopURL = "";
 		readonly int m_HadoopRetryUpload = 0;
         readonly List<KeyValuePair<string, string>> m_URLSwitch = null;
+
+        static object m_Lock = new object();
+
+        class HadoopUploadProgress
+        {
+            public string filePath;
+            public long position;
+            public long size;
+        }
+
+        static Dictionary<string, HadoopUploadProgress> m_HadoopUploadProgress = new Dictionary<string, HadoopUploadProgress>();
 
         public HomeController(IHostingEnvironment hostingEnvironment
                             , IConfiguration configuration
@@ -69,12 +81,33 @@ namespace aspcorehadoopupload.Controllers
             return Content(JsonConvert.SerializeObject(new { result = result, }));
         }
 
+
+        [Route("HadoopCheckStatus"), HttpPost]
+        public IActionResult HadoopCheckStatus(string key)
+        {
+            HadoopUploadProgress hadoopUploadProgress = null;
+
+            lock(m_Lock)
+            {
+                if ( m_HadoopUploadProgress.TryGetValue(key, out hadoopUploadProgress) )
+                {
+                }
+            }
+
+            if (hadoopUploadProgress == null) goto __FAILED;
+
+            return Content(JsonConvert.SerializeObject(new { result = "OK", data = JsonConvert.SerializeObject(hadoopUploadProgress), }));
+__FAILED:
+            return Content(JsonConvert.SerializeObject(new { result = "NONE", }));
+        }
+
+
 		//[RequestSizeLimit(2000000000)]
 		//[RequestFormLimits(ValueLengthLimit = 2000000000, MultipartBodyLengthLimit = 2000000000)]
 		[DisableRequestSizeLimit]
 		[RequestFormLimits(ValueLengthLimit = int.MaxValue, MultipartBodyLengthLimit = long.MaxValue)]
-		[Route("UploadVideo"), HttpPost]
-		public async Task<IActionResult> UploadVideo(string path, IFormFile fileUpload)
+		[Route("UploadFile"), HttpPost]
+		public async Task<IActionResult> UploadFile(string path, IFormFile fileUpload)
         {
             string failedType = string.Empty;
 			
@@ -114,44 +147,119 @@ namespace aspcorehadoopupload.Controllers
             // Console.WriteLine("path : " + path);
             // Console.WriteLine("fileUpload.Name : " + fileUpload.Name);
 
+            long size = 0;
             using(var fs = System.IO.File.OpenRead(tempFile))
             {
-                byte[] buffer = new byte[10000000];
-                int result = 0;
-                Console.WriteLine("Hadoop URL Upload : " + targetPath);
-
-                {
-                    bool upload = await HadoopAPI.UploadFile(m_HadoopURL, targetPath, m_URLSwitch, null);
-                    if (!upload)
-                    {
-                        failedType = "failed upload to hadoop";
-                        goto __FAILED_DELETE_TEMP_FILE;
-                    }
-                }
-
-                while ((result = await fs.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                {
-                    //Console.WriteLine("Pos : " + fs.Position);
-                    
-                    bool upload = await HadoopAPI.UploadFileAppend(m_HadoopURL, targetPath, m_URLSwitch, buffer);
-                    if (!upload)
-                    {
-                        failedType = "failed write append to hadoop";
-                        goto __FAILED_DELETE_TEMP_FILE;
-                    }
-                }
+                size = fs.Length;
             }
 
+            string key = Guid.NewGuid().ToString();
+            HadoopUploadProgress hadoopUploadProgress = new HadoopUploadProgress(){ filePath = tempFile, position = 0, size = size, };
+            lock(m_Lock)
+            {
+                m_HadoopUploadProgress.Add(key, hadoopUploadProgress);
+            }
+
+            Thread thread = new Thread(new ParameterizedThreadStart( async (object arg)=> {
+                HadoopUploadProgress hup = (HadoopUploadProgress)arg;
+                //Console.WriteLine(JsonConvert.SerializeObject(hup));
+
+                using(var fs = System.IO.File.OpenRead(hup.filePath))
+                {
+                    byte[] buffer = new byte[10000000];
+                    int result = 0;
+                    Console.WriteLine("Hadoop URL Upload : " + targetPath);
+
+                    {
+                        bool upload = await HadoopAPI.UploadFile(m_HadoopURL, targetPath, m_URLSwitch, null);
+                        if (!upload)
+                        {
+                            failedType = "failed upload to hadoop";
+                            goto __END;
+                        }
+                    }
+
+                    while ((result = await fs.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        //Console.WriteLine("Pos : " + fs.Position);
+                    
+                        bool upload = await HadoopAPI.UploadFileAppend(m_HadoopURL, targetPath, m_URLSwitch, buffer);
+                        if (!upload)
+                        {
+                            failedType = "failed write append to hadoop";
+                            goto __END;
+                        }
+
+                        lock(m_Lock)
+                        {
+                            hup.position = fs.Position;
+                        }
+                    }
+
+                    lock(m_Lock)
+                    {
+                        m_HadoopUploadProgress.Remove(key);
+                    }
+                }
+
+__END:
+                System.IO.File.Delete(hup.filePath);
+            }));
+            thread.Start(hadoopUploadProgress);
+
+            
+//             await Task.Run( async () => {
+//                 //Console.WriteLine(JsonConvert.SerializeObject(hup));
+
+//                 using(var fs = System.IO.File.OpenRead(hup.filePath))
+//                 {
+//                     byte[] buffer = new byte[10000000];
+//                     int result = 0;
+//                     Console.WriteLine("Hadoop URL Upload : " + targetPath);
+
+//                     {
+//                         bool upload = await HadoopAPI.UploadFile(m_HadoopURL, targetPath, m_URLSwitch, null);
+//                         if (!upload)
+//                         {
+//                             failedType = "failed upload to hadoop";
+//                             goto __END;
+//                         }
+//                     }
+
+//                     while ((result = await fs.ReadAsync(buffer, 0, buffer.Length)) > 0)
+//                     {
+//                         //Console.WriteLine("Pos : " + fs.Position);
+                        
+//                         bool upload = await HadoopAPI.UploadFileAppend(m_HadoopURL, targetPath, m_URLSwitch, buffer);
+//                         if (!upload)
+//                         {
+//                             failedType = "failed write append to hadoop";
+//                             goto __END;
+//                         }
+
+//                         lock(m_Lock)
+//                         {
+//                             hup.position = fs.Position;
+//                             hup.length = buffer.Length;
+//                         }
+//                     }
+
+//                     lock(m_Lock)
+//                     {
+//                         m_HadoopUploadProgress.Remove(key);
+//                     }
+//                 }
+
+// __END:
+//                 System.IO.File.Delete(hup.filePath);
+//             });
+
 
             
 
             
 
-            System.IO.File.Delete(tempFile);
-            return Content(JsonConvert.SerializeObject(new { result = "OK", }));
-
-__FAILED_DELETE_TEMP_FILE:
-            System.IO.File.Delete(tempFile);
+            return Content(JsonConvert.SerializeObject(new { result = "OK", key = key, }));
 
 __FAILED:
             return Content(JsonConvert.SerializeObject(new { result = "FAILED", failedType = failedType, }));
